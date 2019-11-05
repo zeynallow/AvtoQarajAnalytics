9eb9818e1c90db2b773e1efa412d79760df03d3b
Zeynallow <zeynaloffnet@gmail.com>
2019-11-05 10:46:48 +0400

    disable register fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,8 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-      abort(400);
-
+      abort(404);
         // return User::create([
         //     'name' => $data['name'],
         //     'email' => $data['email'],
