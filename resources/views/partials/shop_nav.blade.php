28fc9ddf7c3c17f1e4fb79636e1e129267699257
Zeynallow <zeynaloffnet@gmail.com>
2019-11-12 15:52:55 +0400

    hide reports

--- resources/views/partials/shop_nav.blade.php
+++ resources/views/partials/shop_nav.blade.php
@@ -9,6 +9,6 @@
 
   <li class="menu-header">Sosial şəbəkə</li>
   <li class="{{ request()->is('social_reports') ? 'active' : '' }}"><a href="{{ route('social_reports.index') }}"><i class="fa fa-table"></i> <span>Müraciətlər</span></a></li>
-  <li class="{{ request()->is('social_reports/reports') ? 'active' : '' }}"><a href="{{ route('social_reports.reports') }}"><i class="fa fa-chart-pie"></i> <span>Hesabat</span></a></li>
+  {{-- <li class="{{ request()->is('social_reports/reports') ? 'active' : '' }}"><a href="{{ route('social_reports.reports') }}"><i class="fa fa-chart-pie"></i> <span>Hesabat</span></a></li> --}}
 
 </ul>
--- resources/views/partials/staff_nav.blade.php
+++ resources/views/partials/staff_nav.blade.php
@@ -2,5 +2,5 @@
   <li class="menu-header">Sosial şəbəkə</li>
   <li class="{{ request()->is('social_reports/create') ? 'active' : '' }}"><a href="{{ route('social_reports.create') }}"><i class="fa fa-plus"></i> <span>Müraciət yarat</span></a></li>
   <li class="{{ request()->is('social_reports') ? 'active' : '' }}"><a href="{{ route('social_reports.index') }}"><i class="fa fa-table"></i> <span>Müraciətlər</span></a></li>
-  <li class="{{ request()->is('social_reports/reports') ? 'active' : '' }}"><a href="{{ route('social_reports.reports') }}"><i class="fa fa-chart-pie"></i> <span>Hesabat</span></a></li>
+  {{-- <li class="{{ request()->is('social_reports/reports') ? 'active' : '' }}"><a href="{{ route('social_reports.reports') }}"><i class="fa fa-chart-pie"></i> <span>Hesabat</span></a></li> --}}
 </ul>
