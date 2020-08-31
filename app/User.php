3f0ec0f7e2c1f8d558ab251acd3fc7e4ec5053a6
anarzone <haqverdiyevanar@gmail.com>
2020-08-31 16:47:23 +0400

    AA-7: Created AuthController, login, logout, refresh methods. Created routes for AuthController. Updated User model

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
   use Notifiable;
 
@@ -45,4 +46,13 @@ class User extends Authenticatable
     return $this->hasMany('App\ShopUserCar','user_id');
   }
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
