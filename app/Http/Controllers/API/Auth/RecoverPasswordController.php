53cb9ec3ed0d799b5cc029169cc48efe383ae44d
anarzone <haqverdiyevanar@gmail.com>
2020-09-01 15:19:54 +0400

    AA-9: created password reset with otp endpoint. Added new controller and methods

--- app/Http/Controllers/API/Auth/RecoverPasswordController.php
+++ app/Http/Controllers/API/Auth/RecoverPasswordController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\API\Auth;
 
 use App\Events\SendMail;
 use App\Http\Controllers\Controller;
+use App\OtpAuthentication;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class RecoverPasswordController extends Controller
 {
@@ -14,16 +19,71 @@ class RecoverPasswordController extends Controller
             'email' => 'required|email',
         ]);
 
-        $sent = Event::dispatch(new SendMail(
+        $otp = $this->otpGenerator();
+        $token = Str::random(60);
+
+        OtpAuthentication::create([
+            'otp' => $otp,
+            'email' => $request->email,
+            'token' => Str::random(60),
+        ]);
+
+        Event::dispatch(new SendMail(
             $request->email,
             'Şifrə bərpa kodu',
-           'AB5413',
+            $this->otpGenerator(),
             'app.emails.otp'
         ));
 
         return response()->json([
             'message' => 'success',
-            'data' => $sent
+            'data' => ['token' => $token]
         ]);
+    }
+
+    public function recoverPassword(Request $request){
+        $request->validate([
+            'code' => 'required|max:6',
+            'password' => 'required|confirmed',
+            'token' => 'required|string'
+        ]);
+
+        $otpAuth = OtpAuthentication::where(['token' => $request->token, 'expired' => OtpAuthentication::ACTIVE])->first();
+
+        if($otpAuth){
+            if($otpAuth->code != $request->code){
+                $response = [
+                    'message' => 'error',
+                    'data' => 'OTP şifrə yalnışdır'
+                ];
+                $status = Response::HTTP_BAD_REQUEST;
+            }
+            User::where('email', $otpAuth->email)->update(['password' => Hash::make($request->password)]);
+            $otpAuth->update(['expired' => OtpAuthentication::EXPIRED]);
+
+            $response = [
+                'message' => 'success',
+            ];
+            $status = Response::HTTP_CREATED;
+
+        }else{
+            $response = [
+                'message' => 'error',
+                'data' => 'Token yalnışdır və ya artıq aktiv deyil'
+            ];
+            $status = Response::HTTP_BAD_REQUEST;
+        }
+
+        return response()->json($response, $status);
+    }
+
+    public function otpGenerator(){
+        $alphaNum = range('0','9');
+        shuffle($alphaNum);
+        $otp = '';
+        for($i = 0; $i < 6; $i++){
+            $otp .= $alphaNum[$i];
+        }
+        return $otp;
     }
 }
