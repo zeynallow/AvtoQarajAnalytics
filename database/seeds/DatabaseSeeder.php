f54da5e74e830babfbbfa27854dac42d6207ed27
anarzone <haqverdiyevanar@gmail.com>
2020-10-20 18:16:46 +0400

    Created endpoint to send player id

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SocialReportStatus::class);
         $this->call(SocialReportReply::class);
         $this->call(SocialReportCancel::class);
+        $this->call(TestUserSeeder::class);
     }
 }
